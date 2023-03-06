67a5381edf0f7e148d1cf03ccb0e6318cc6f4d7a
wndgus0468 <118896751+wndgua0468@users.noreply.github.com>
2023-03-06 12:47:52 +0900

    Create 35.2.c

---
+++ 2023.03.03/unit35/35.2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char* s1 = "Hello";
+
+    printf("%c\n", s1[1]);   
+    printf("%c\n", s1[4]);   
+    printf("%c\n", s1[5]);    
+
+    s1[0] = 'A';           
+ 
+
+    printf("%c\n", s1[0]);
+
+  
+    return 0;
+}

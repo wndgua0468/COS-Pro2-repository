8a1efd0a50730ee48db6ced2a67af365e8e8fd2c
wndgus0468 <118896751+wndgua0468@users.noreply.github.com>
2023-03-06 12:35:23 +0900

    Create 34.2.c

---
+++ unit.34/34.2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+int main()
+{
+	int numArr[3][4] = {0, };
+
+	printf("%d\n", numArr[0][0]);
+	printf("%d\n", numArr[1][2]);
+	printf("%d\n", numArr[3][4]);
+	printf("%d\n", numArr[5][6]);
+
+	return 0;
+}

4bb2dc1e44e6bc0aea0625a96368c077fd820c95
wndgus0468 <118896751+wndgua0468@users.noreply.github.com>
2023-03-06 12:44:17 +0900

    Update 34.1.c

--- 2023.03.03/unit34/34.1.c
+++ 2023.03.03/unit34/34.1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int numArr[3][4] = {
+        { 12, 13, 14, 15 },
+        { 55, 66, 77, 88 },
+        { 99, 110, 121, 132 }
+    };
+
+    printf("%d\n", numArr[0][0]);
+    printf("%d\n", numArr[1][2]);
+    printf("%d\n", numArr[2][0]);
+    printf("%d\n", numArr[2][3]);
+
+
+    return 0;
+}
 
